af0e0f875ec0db40383b18d1d95c08eb2acdd8c3
Student <example@example.com>
2019-12-11 11:01:07 -0800

    add tie functionality tests

--- spec/rock_paper_scissors_engine_spec.rb
+++ spec/rock_paper_scissors_engine_spec.rb
@@ -11,16 +11,28 @@ require('rock_paper_scissors_engine')
       test = Engine.new("scissors")
       expect(test.rocks_engine()).to(eq(1))
     end
-    it('determines that rock is the winner if computer chooses scissors') do
+    it('determines that rock is the winner if opponent chooses scissors') do
       test = Engine.new("scissors")
       expect(test.rocks_engine()).to(eq(["winner!"]))
     end
-    it('determines that paper is the winner if computer chooses rock') do
+    it('determines that paper is the winner if opponent chooses rock') do
       test = Engine.new("rock")
       expect(test.papers_engine()).to(eq(["winner!"]))
     end
-    it('determines that scissors is the winner if computer chooses paper') do
+    it('determines that scissors is the winner if opponent chooses paper') do
       test = Engine.new("paper")
       expect(test.scissors_engine()).to(eq(["winner!"]))
+    end
+    it('determines a tie if player and opponent both choose rock') do
+      test = Engine.new("rock")
+      expect(test.rocks_engine()).to(eq(["tie!"]))
+    end
+    it('determines a tie if player and opponent both choose paper') do
+      test = Engine.new("paper")
+      expect(test.papers_engine()).to(eq(["tie!"]))
+    end
+    it('determines a tie if player and opponent both choose scissors') do
+      test = Engine.new("scissors")
+      expect(test.scissors_engine()).to(eq(["tie!"]))
     end
   end
