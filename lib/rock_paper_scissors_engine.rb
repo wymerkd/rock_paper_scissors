4bd8e91463c820da7369bb25b8a77902747fae02
Student <example@example.com>
2019-12-11 11:17:15 -0800

    create instance for resultarray and remove from each method.

--- lib/rock_paper_scissors_engine.rb
+++ lib/rock_paper_scissors_engine.rb
@@ -2,54 +2,48 @@ class Engine
 
   def initialize(choice)
     @choice = choice
+    @result_array1 = []
   end
 
   def choice
     @choice
+    @result_array1 = []
   end
 
   def rocks_engine
     rock_result = { "scissors" => 1, "rock" => 2, "paper" => 3}
-    result_array1 = []
     choice_find = rock_result.fetch(@choice)
-    choice_find
     if (choice_find === 2)
-      result_array1.push('tie!')
+      @result_array1.push('tie!')
     elsif (choice_find === 3)
-      result_array1.push('loser!')
+      @result_array1.push('loser!')
     else
-      result_array1.push('winner!')
+      @result_array1.push('winner!')
     end
-    return result_array1
   end
 
   def papers_engine
     paper_result = { "rock" => 1, "paper" => 2, "scissors" => 3}
-    result_array1 = []
     choice_find = paper_result.fetch(@choice)
-    choice_find
     if (choice_find === 2)
-      result_array1.push('tie!')
+      @result_array1.push('tie!')
     elsif (choice_find === 3)
-      result_array1.push('loser!')
+      @result_array1.push('loser!')
     else
-      result_array1.push('winner!')
+      @result_array1.push('winner!')
     end
-    return result_array1
   end
 
   def scissors_engine
     scissors_result = { "paper" => 1, "scissors" => 2, "rock" => 3}
-    result_array1 = []
     choice_find = scissors_result.fetch(@choice)
-    choice_find
     if (choice_find === 2)
-      result_array1.push('tie!')
+      @result_array1.push('tie!')
     elsif (choice_find === 3)
-      result_array1.push('loser!')
+      @result_array1.push('loser!')
     else
-      result_array1.push('winner!')
+      @result_array1.push('winner!')
     end
-    return result_array1
   end
+  @result_array1
 end
--- spec/rock_paper_scissors_engine_spec.rb
+++ spec/rock_paper_scissors_engine_spec.rb
@@ -3,14 +3,14 @@ require('rock_paper_scissors_engine')
 
 
   describe('#rock_paper_scissors_engine') do
-    it('makes the initialize work') do
-      test = Engine.new("123")
-      expect(test.rocks_engine()).to(eq("123"))
-    end
-    it('determines the number value of a user choice') do
-      test = Engine.new("scissors")
-      expect(test.rocks_engine()).to(eq(1))
-    end
+    # it('makes the initialize work') do
+    #   test = Engine.new("123")
+    #   expect(test.rocks_engine()).to(eq("123"))
+    # end
+    # it('determines the number value of a user choice') do
+    #   test = Engine.new("scissors")
+    #   expect(test.rocks_engine()).to(eq(1))
+    # end
     it('determines that rock is the winner if opponent chooses scissors') do
       test = Engine.new("scissors")
       expect(test.rocks_engine()).to(eq(["winner!"]))
@@ -34,5 +34,17 @@ require('rock_paper_scissors_engine')
     it('determines a tie if player and opponent both choose scissors') do
       test = Engine.new("scissors")
       expect(test.scissors_engine()).to(eq(["tie!"]))
+    end
+    it('determines a loss if player chooses rock and opponent chooses paper') do
+      test = Engine.new("paper")
+      expect(test.rocks_engine()).to(eq(["loser!"]))
+    end
+    it('determines a loss if player chooses paper and opponent chooses scissors') do
+      test = Engine.new("scissors")
+      expect(test.papers_engine()).to(eq(["loser!"]))
+    end
+    it('determines a loss if player chooses scissors and opponent chooses rock') do
+      test = Engine.new("rock")
+      expect(test.scissors_engine()).to(eq(["loser!"]))
     end
   end
