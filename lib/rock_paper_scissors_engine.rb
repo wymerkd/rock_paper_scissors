e021fac56ffbe154d0ccc4150ce083ab0b242b26
Student <example@example.com>
2019-12-11 10:54:59 -0800

    add full back-end, add partial tests

--- lib/rock_paper_scissors_engine.rb
+++ lib/rock_paper_scissors_engine.rb
@@ -8,14 +8,48 @@ class Engine
     @choice
   end
 
-  def rps_network
-    rock_result = { "scissors" => 1, "rock" => 2, "paper" => 3, "123" => "123" }
-    # result_array === []
+  def rocks_engine
+    rock_result = { "scissors" => 1, "rock" => 2, "paper" => 3}
+    result_array1 = []
     choice_find = rock_result.fetch(@choice)
     choice_find
-  #   if (rock_result.@choice === 2)
-  #     result_array.push('tie!')
-  #   else
-  #     result_array.push('loser!')
+    if (choice_find === 2)
+      result_array1.push('tie!')
+    elsif (choice_find === 3)
+      result_array1.push('loser!')
+    else
+      result_array1.push('winner!')
+    end
+    return result_array1
+  end
+
+  def papers_engine
+    paper_result = { "rock" => 1, "paper" => 2, "scissors" => 3}
+    result_array1 = []
+    choice_find = paper_result.fetch(@choice)
+    choice_find
+    if (choice_find === 2)
+      result_array1.push('tie!')
+    elsif (choice_find === 3)
+      result_array1.push('loser!')
+    else
+      result_array1.push('winner!')
+    end
+    return result_array1
+  end
+
+  def scissors_engine
+    scissors_result = { "paper" => 1, "scissors" => 2, "rock" => 3}
+    result_array1 = []
+    choice_find = scissors_result.fetch(@choice)
+    choice_find
+    if (choice_find === 2)
+      result_array1.push('tie!')
+    elsif (choice_find === 3)
+      result_array1.push('loser!')
+    else
+      result_array1.push('winner!')
+    end
+    return result_array1
   end
 end
--- spec/rock_paper_scissors_engine_spec.rb
+++ spec/rock_paper_scissors_engine_spec.rb
@@ -5,10 +5,22 @@ require('rock_paper_scissors_engine')
   describe('#rock_paper_scissors_engine') do
     it('makes the initialize work') do
       test = Engine.new("123")
-      expect(test.rps_network()).to(eq("123"))
+      expect(test.rocks_engine()).to(eq("123"))
+    end
+    it('determines the number value of a user choice') do
+      test = Engine.new("scissors")
+      expect(test.rocks_engine()).to(eq(1))
     end
     it('determines that rock is the winner if computer chooses scissors') do
       test = Engine.new("scissors")
-      expect(test.rps_network()).to(eq(1))
+      expect(test.rocks_engine()).to(eq(["winner!"]))
+    end
+    it('determines that paper is the winner if computer chooses rock') do
+      test = Engine.new("rock")
+      expect(test.papers_engine()).to(eq(["winner!"]))
+    end
+    it('determines that scissors is the winner if computer chooses paper') do
+      test = Engine.new("paper")
+      expect(test.scissors_engine()).to(eq(["winner!"]))
     end
   end
